3e5c003fdc95784723c42a40cc1985ae32509769
Parth Kalbag <10bparth@gmail.com>
2021-04-06 14:39:12 +0530

    Testing URLs added

---
+++ rest/tests/test_urls.py
@@ -0,0 +1,38 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from shop.views import *
+
+class TestUrls(SimpleTestCase):
+    def test_index_is_resolved(self):
+        url = reverse('ShopHome')
+        self.assertEquals(resolve(url).func, index)
+        
+    def test_about_is_resolved(self):
+        url = reverse('AboutUs')
+        self.assertEquals(resolve(url).func, about)
+
+    def test_contact_is_resolved(self):
+        url = reverse('ContactUs')
+        self.assertEquals(resolve(url).func, contact)
+        
+    def test_tracker_is_resolved(self):
+        url = reverse('TrackingStatus')
+        self.assertEquals(resolve(url).func, tracker)   
+     
+    def test_search_is_resolved(self):
+        url = reverse('search')
+        self.assertEquals(resolve(url).func, search)
+
+    def test_product_id_is_resolved(self):
+        url = reverse('ProductView', args=[5])
+        self.assertEquals(resolve(url).func, productview)
+        
+    def test_checkout_is_resolved(self):
+        url = reverse('Checkout')
+        self.assertEquals(resolve(url).func, checkout)
+
+    def test_faq_is_resolved(self):
+        url = reverse('faq')
+        self.assertEquals(resolve(url).func, faq)
+
+    
